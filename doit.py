2b99d34f870f4ddfbdb6844e09633731e46087de
Ryan Bergman <ryan.bergman@gmail.com>
2018-05-15 07:00:43 -0500

    Create doit.py

---
+++ doit.py
@@ -0,0 +1,9 @@
+import praw
+
+
+
+reddit = praw.Reddit(client_id='dQJdVsQ5cf-kIg',
+                     client_secret='xxxxx--replaceme--xxxxx',
+                     password='xxxx---replacemetoo----xxxx',
+                     user_agent='testscript by /u/fakebot3',
+                     username='ryberrr')
